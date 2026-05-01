e56ebbf2ac75e2ad4b634916fa7943ffb766b680
Marwan Alaa <marwan.alaa2002@gmail.com>
2026-05-01 17:13:01 +0300

    Adding InitController new requests method to create store/update requests for each model

--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,13 @@ Route::prefix('init')->controller(InitController::class)->group(function () {
     Route::get('models', 'models');
     Route::get('controllers', 'controllers');
     Route::get('factories', 'factories');
-Route::get('seeders', 'seeders');
+    Route::get('seeders', 'seeders');
+    Route::get('requests', 'requests');
 });
 
 Route::prefix('auth')->controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('login', 'login');
     Route::post('logout', 'logout')->middleware('auth:sanctum');
+    
 });
